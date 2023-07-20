5b9ffb6f58d9801cd621caf02e613ec624169b16
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-07-20 19:51:19 +0300

    removed visible backend

--- workwise_server/app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < Sinatra::Base
-    set :default_content_type, 'application/json'
-  
-    get '/' do
-      { name: 'Hello World' }.to_json
-    end
-  
-  end
--- workwise_server/config/environment.rb
+++
@@ -1,8 +0,0 @@
-ENV["RACK_ENV"] ||= "development"
-
-require 'bundler/setup'
-Bundler.require(:default, ENV["RACK_ENV"])
-
-require 'sinatra/activerecord/rake'
-
-require_all 'app'
